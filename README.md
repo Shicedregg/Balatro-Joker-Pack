48e93a78e7b70630fdc921d54c2f91e1307f295f
Shicedregg <jakobgeib@gmail.com>
2024-02-29 02:23:11 +0100

    added restaurant joker + wip sprites, updated readme

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # Balatro-Joker-Pack
 
-This mod adds three Jokers to Balatro
+This mod adds the following four Jokers to Balatro:
 
-requires [Steamodded](https://github.com/Steamopollys/Steamodded) to work
+1. Good Fortune - provides you with an extra bountiful +77 Chips
+
+2. 1337 h4x0r - this little feller pleases you with +1337 Mult
+
+3. Ace up the Sleeve - if you have exactly one Ace on hand, gain x11 Mult
+
+4. Restaurant - blesses you with +15 Mult for each other food Joker and even goes so far as to showering you with x25 Mult if all other Jokers are food
+
+## Installation
+
+- download [Steamodded](https://github.com/Steamopollys/Steamodded)
+- drag and drop your "Balatro.exe" to the above mentioned executable
+- copy the 'ShiceJokerPack' folder into `C:\Users\<USER>\AppData\Roaming\Balatro\Mods` (or `%appdata%\Balatro\Mods`) (create the "Mods" directory if necessary)
+- launch the game
--- ShiceJokerPack/ShiceJokerPack.lua
+++ ShiceJokerPack/ShiceJokerPack.lua
@@ -13,6 +13,17 @@ function table_length(table)
     return count
 end
 
+function has_value (tab, val)
+    for index, value in ipairs(tab) do
+        if value == val then
+            return true
+        end
+    end
+    return false
+end
+
+food = {"Gros Michel", "Egg", "Ice Cream", "Cavendish", "Turtle Bean", "Diet Cola", "Popcorn", "Ramen", "Seltzer"}
+
 function SMODS.INIT.JokerPack()
     local jokerTable = {
         -- Good Fortune blesses your soul with a considerate amount of chips!
@@ -56,6 +67,20 @@ function SMODS.INIT.JokerPack()
             set = "Joker",
             config = { extra = 11 },
             pos = { x = 2, y = 16 },
+        },
+        -- Can't win on an empty stomach
+        j_restaurant = {
+            order = 0,
+            unlocked = true,
+            discovered = true,
+            blueprint_compat = true,
+            eternal_compat = true,
+            rarity = 1,
+            cost = 4,
+            name = "Restaurant",
+            set = "Joker",
+            config = { extra = {mult = 15, x_mult = 25} },
+            pos = { x = 3, y = 16 },
         }
     }
 
@@ -89,6 +114,14 @@ function SMODS.INIT.JokerPack()
             text = {
                 "{X:mult,C:white} X#1# {} Mult if you",
                 "have a single {C:attention}Ace{} on hand"
+            }
+        },
+        j_restaurant = {
+            name = "Restaurant",
+            text = {
+                "{C:mult}+#1#{} Mult for each other food",
+                "Joker, {X:mult,C:white} X#2# {} Mult if all",
+                "other Jokers are food"
             }
         }
     }
@@ -127,16 +160,6 @@ function SMODS.INIT.JokerPack()
     sprite_jokerpack:register()
 end
 
---[[ Joker abilities
-local set_abilityref = Card.set_ability
-function Card.set_ability(self, center, initial, delay_sprites)
-    set_abilityref(self, center, initial, delay_sprites)
-    if self.ability.name == "1337 h4x0r" then
-        self.ability.mult = 1337
-    end
-end
---]]
-
 local calculate_jokerref = Card.calculate_joker
 function Card.calculate_joker(self, context)
     local calc_ref = calculate_jokerref(self, context)
@@ -171,6 +194,23 @@ function Card.calculate_joker(self, context)
             if context.cardarea == G.hand then
             end
         elseif context.other_joker then
+            if self.ability.name == "Restaurant" then
+                local food_available = 0
+                local jokers = 1
+                if self.ability.name == "Restaurant" and has_value(food, context.other_joker.ability.name) and self ~= context.other_joker then
+                    food_available = food_available + 1
+                    G.E_MANAGER:add_event(Event({
+                        func = function()
+                            context.other_joker:juice_up(0.5, 0.5)
+                            return true
+                        end
+                    })) 
+                    return {
+                        message = localize{type='variable',key='a_mult',vars={self.ability.extra.mult}},
+                        mult_mod = self.ability.extra.mult
+                    }
+                end
+            end
         else
             if context.cardarea == G.jokers then
                 if context.before then
@@ -197,6 +237,20 @@ function Card.calculate_joker(self, context)
                             return {
                                 message = localize{type='variable',key='a_xmult',vars={self.ability.extra}},
                                 Xmult_mod = self.ability.extra
+                            }
+                        end
+                    end
+                    if self.ability.name == "Restaurant" then
+                        local food_jokers_in_hand = 0
+                        local jokers_in_hand = 0
+                        for i=1, #G.jokers.cards do
+                            jokers_in_hand = jokers_in_hand + 1
+                            if has_value(food, G.jokers.cards[i].ability.name) then food_jokers_in_hand = food_jokers_in_hand + 1 end
+                        end
+                        if ((food_jokers_in_hand) == (jokers_in_hand - 1)) and (jokers_in_hand >= 2) then
+                            return {
+                                message = localize{type='variable',key='a_xmult',vars={self.ability.extra.x_mult}},
+                                Xmult_mod = self.ability.extra.x_mult
                             }
                         end
                     end
@@ -229,6 +283,9 @@ function Card.generate_UIBox_ability_table(self)
             customJoker = true
         elseif self.ability.name == "Ace up the Sleeve" then
             loc_vars = {self.ability.extra}
+            customJoker = true
+        elseif self.ability.name == "Restaurant" then
+            loc_vars = {self.ability.extra.mult, self.ability.extra.x_mult}
             customJoker = true
         end
 
